876c9cbccddd12b6f8e4afb80bdcff2b6d3aa4ff
annalia01 <annalia.ruggiero01@gmail.com>
2025-05-07 13:08:14 +0200

    Update debug_interfaces.sv

--- debug_module/debug_interfaces.sv
+++ debug_module/debug_interfaces.sv
@@ -15,6 +15,32 @@ interface dmi_interface;
             input address, jtag_data, new_request, rnw);
 endinterface
 
+typedef struct packed {
+    logic [6:0] address;
+    logic [31:0] jtag_data;
+    logic new_request;
+    logic rnw;
+} jtag_dmi_interface_output;
+
+typedef struct packed {
+    logic handled;
+    logic [1:0] response;
+    logic [31:0] dmi_data;
+} jtag_dmi_interface_input;
+
+typedef struct packed {
+    logic handled;
+    logic [1:0] response;
+    logic [31:0] dmi_data;
+} dmi_dmi_interface_output;
+
+typedef struct packed {
+    logic [6:0] address;
+    logic [31:0] jtag_data;
+    logic new_request;
+    logic rnw;
+} dmi_dmi_interface_input;
+
 
 interface dmi_cpu_interface;
     logic halt;
@@ -43,3 +69,49 @@ interface dmi_cpu_interface;
             output halt_ack, resume_ack,program_buffer_addr, reset_ack,running,halt_ack_recv,resume_ack_recv,read_data,rnw_ack,
             input halt, resume, program_buffer_data,reset,write_data,rnw_new_request,rnw,read_write_addr);
 endinterface  
+
+typedef struct packed {
+    logic halt;
+    logic resume;
+    logic reset;
+    logic [31:0] write_data;
+    logic [31:0] read_write_addr;
+    logic rnw;
+    logic rnw_new_request;
+    logic [31:0] program_buffer_data;
+} dmi_dmi_cpu_interface_output;
+
+typedef struct packed {
+    logic halt_ack;
+    logic resume_ack;
+    logic reset_ack;
+    logic halt_ack_recv;
+    logic resume_ack_recv;
+    logic running;
+    logic [31:0] read_data;
+    logic rnw_ack;
+    logic [3:0] program_buffer_addr;
+} dmi_dmi_cpu_interface_input;
+
+typedef struct packed {
+    logic halt_ack;
+    logic resume_ack;
+    logic reset_ack;
+    logic halt_ack_recv;
+    logic resume_ack_recv;
+    logic running;
+    logic [31:0] read_data;
+    logic rnw_ack;
+    logic [3:0] program_buffer_addr;
+} cpu_dmi_cpu_interface_output;
+
+typedef struct packed {
+    logic halt;
+    logic resume;
+    logic reset;
+    logic [31:0] write_data;
+    logic [31:0] read_write_addr;
+    logic rnw;
+    logic rnw_new_request;
+    logic [31:0] program_buffer_data;
+} cpu_dmi_cpu_interface_input;
