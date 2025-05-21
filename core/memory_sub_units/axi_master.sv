5a3250ae5cae20acd63dea8d721eeb05304519e0
annalia01 <annalia.ruggiero01@gmail.com>
2025-05-21 11:06:51 +0200

    Update axi_master.sv

--- core/memory_sub_units/axi_master.sv
+++ core/memory_sub_units/axi_master.sv
@@ -30,12 +30,19 @@ module axi_master
         input logic rst,
 
         output logic write_outstanding,
-        axi_interface.master m_axi,
+        //axi_interface.master m_axi,
+        master_axi_interface_input m_axi_input,
+        master_axi_interface_output m_axi_output,
 
         input logic amo,
         input amo_t amo_type,
-        amo_interface.subunit amo_unit,
-        memory_sub_unit_interface.responder ls
+        //amo_interface.subunit amo_unit,
+        subunit_amo_interface_input amo_unit_input,
+        subunit_amo_interface_output amo_unit_output,
+        
+        //memory_sub_unit_interface.responder ls
+        responder_memory_sub_unit_interface_input ls_input,
+        responder_memory_sub_unit_interface_output ls_output
     );
 
     ////////////////////////////////////////////////////
@@ -51,83 +58,83 @@ module axi_master
     state_t current_state;
 
     logic request_is_invalid_sc;
-    assign request_is_invalid_sc = amo & amo_type == AMO_SC_FN5 & ~amo_unit.reservation_valid;
+    assign request_is_invalid_sc = amo & amo_type == AMO_SC_FN5 & ~amo_unit_input.reservation_valid;
 
-    assign amo_unit.set_reservation = ls.new_request & amo & amo_type == AMO_LR_FN5;
-    assign amo_unit.clear_reservation = ls.new_request;
-    assign amo_unit.reservation = ls.addr;
-    assign amo_unit.rs1 = ls.data_out;
+    assign amo_unit_output.set_reservation = ls_input.new_request & amo & amo_type == AMO_LR_FN5;
+    assign amo_unit_output.clear_reservation = ls_input.new_request;
+    assign amo_unit_output.reservation = ls_input.addr;
+    assign amo_unit_output.rs1 = ls_output.data_out;
 
     logic[29:0] addr;
-    assign m_axi.awaddr = {addr, 2'b0};
-    assign m_axi.araddr = {addr, 2'b0};
-    assign m_axi.awlen = '0;
-    assign m_axi.arlen = '0;
-    assign m_axi.awburst = '0;
-    assign m_axi.arburst = '0;
-    assign m_axi.awid = '0;
-    assign m_axi.arid = '0;
-    assign m_axi.rready = 1;
-    assign m_axi.bready = 1;
+    assign m_axi_output.awaddr = {addr, 2'b0};
+    assign m_axi_output.araddr = {addr, 2'b0};
+    assign m_axi_output.awlen = '0;
+    assign m_axi_output.arlen = '0;
+    assign m_axi_output.awburst = '0;
+    assign m_axi_output.arburst = '0;
+    assign m_axi_output.awid = '0;
+    assign m_axi_output.arid = '0;
+    assign m_axi_output.rready = 1;
+    assign m_axi_output.bready = 1;
 
     always_ff @(posedge clk) begin
         unique case (current_state)
             READY : begin //Accept any request
-                ls.ready <= ~ls.new_request | request_is_invalid_sc;
-                ls.data_out <= 1;
-                ls.data_valid <= ls.new_request & request_is_invalid_sc;
-                addr <= ls.addr[31:2];
-                m_axi.awlock <= amo & amo_type != AMO_LR_FN5; //Used in WAITING_READ to determine if it was a RMW
-                m_axi.awvalid <= ls.new_request & (ls.we | (amo & amo_type == AMO_SC_FN5 & amo_unit.reservation_valid));
-                m_axi.arlock <= amo & amo_type != AMO_SC_FN5; //Used in WAITING_WRITE to determine if it was a RNW
-                m_axi.arvalid <= ls.new_request & ls.re & ~(amo & amo_type == AMO_SC_FN5);
-                m_axi.wvalid <= ls.new_request & (ls.we | (amo & amo_type == AMO_SC_FN5 & amo_unit.reservation_valid));
-                m_axi.wdata <= ls.data_in;
-                m_axi.wstrb <= ls.be;
-                write_outstanding <= ls.new_request & (ls.we | amo);
-                amo_unit.rmw_valid <= 0;
-                amo_unit.op <= amo_type;
-                amo_unit.rs2 <= ls.data_in; //Cannot use wdata because wdata will be overwritten if the RMW is not exclusive
-                if (ls.new_request & (ls.we | (amo & amo_type == AMO_SC_FN5 & amo_unit.reservation_valid)))
+                ls_output.ready <= ~ls_input.new_request | request_is_invalid_sc;
+                ls_output.data_out <= 1;
+                ls_output.data_valid <= ls_input.new_request & request_is_invalid_sc;
+                addr <= ls_input.addr[31:2];
+                m_axi_output.awlock <= amo & amo_type != AMO_LR_FN5; //Used in WAITING_READ to determine if it was a RMW
+                m_axi_output.awvalid <= ls_input.new_request & (ls_input.we | (amo & amo_type == AMO_SC_FN5 & amo_unit_input.reservation_valid));
+                m_axi_output.arlock <= amo & amo_type != AMO_SC_FN5; //Used in WAITING_WRITE to determine if it was a RNW
+                m_axi_output.arvalid <= ls_input.new_request & ls_input.re & ~(amo & amo_type == AMO_SC_FN5);
+                m_axi_output.wvalid <= ls_input.new_request & (ls_input.we | (amo & amo_type == AMO_SC_FN5 & amo_unit_input.reservation_valid));
+                m_axi_output.wdata <= ls_input.data_in;
+                m_axi_output.wstrb <= ls_input.be;
+                write_outstanding <= ls_input.new_request & (ls_input.we | amo);
+                amo_unit_output.rmw_valid <= 0;
+                amo_unit_output.op <= amo_type;
+                amo_unit_output.rs2 <= ls_input.data_in; //Cannot use wdata because wdata will be overwritten if the RMW is not exclusive
+                if (ls_input.new_request & (ls_input.we | (amo & amo_type == AMO_SC_FN5 & amo_unit_input.reservation_valid)))
                     current_state <= REQUESTING_WRITE;
-                else if (ls.new_request & ~request_is_invalid_sc)
+                else if (ls_input.new_request & ~request_is_invalid_sc)
                     current_state <= REQUESTING_READ;
             end
             REQUESTING_READ : begin //Wait for read to be accepted
-                m_axi.arvalid <= ~m_axi.arready;
-                if (m_axi.arready)
+                m_axi_output.arvalid <= ~m_axi_input.arready;
+                if (m_axi_input.arready)
                     current_state <= WAITING_READ;
             end
             WAITING_READ : begin //Wait for read response
-                ls.ready <= m_axi.rvalid & ~m_axi.awlock;
-                ls.data_out <= m_axi.rdata;
-                ls.data_valid <= m_axi.rvalid;
-                amo_unit.rmw_valid <= m_axi.rvalid;
-                if (m_axi.rvalid)
-                    current_state <= m_axi.awlock ? REQUESTING_AMO_M : READY;
+                ls_output.ready <= m_axi_input.rvalid & ~m_axi_output.awlock;
+                ls_output.data_out <= m_axi_input.rdata;
+                ls_output.data_valid <= m_axi_input.rvalid;
+                amo_unit_output.rmw_valid <= m_axi_input.rvalid;
+                if (m_axi_input.rvalid)
+                    current_state <= m_axi_output.awlock ? REQUESTING_AMO_M : READY;
             end
             REQUESTING_AMO_M : begin //One cycle for computing the AMO write value
-                ls.data_valid <= 0;
-                m_axi.awvalid <= 1;
-                m_axi.wvalid <= 1;
-                m_axi.wdata <= amo_unit.rd;
-                amo_unit.rmw_valid <= 0;
+                ls_output.data_valid <= 0;
+                m_axi_output.awvalid <= 1;
+                m_axi_output.wvalid <= 1;
+                m_axi_output.wdata <= amo_unit_input.rd;
+                amo_unit_output.rmw_valid <= 0;
                 current_state <= REQUESTING_WRITE;
             end
             REQUESTING_WRITE : begin //Wait for write (address and data) to be accepted
-                m_axi.awvalid <= m_axi.awvalid & ~m_axi.awready;
-                m_axi.wvalid <= m_axi.wvalid & ~m_axi.wready;
-                if ((~m_axi.awvalid | m_axi.awready) & (~m_axi.wvalid | m_axi.wready))
+                m_axi_output.awvalid <= m_axi_output.awvalid & ~m_axi_output.awready;
+                m_axi_output.wvalid <= m_axi_output.wvalid & ~m_axi_output.wready;
+                if ((~m_axi_output.awvalid | m_axi_output.awready) & (~m_axi_output.wvalid | m_axi_output.wready))
                     current_state <= WAITING_WRITE;
             end
             WAITING_WRITE : begin //Wait for write response; resubmit if RMW was not exclusive
-                ls.ready <= m_axi.bvalid & (~m_axi.arlock | m_axi.bresp == 2'b01);
-                ls.data_out <= {31'b0, m_axi.bresp != 2'b01};
-                ls.data_valid <= m_axi.bvalid & m_axi.awlock & ~m_axi.arlock;
-                m_axi.arvalid <= m_axi.bvalid & m_axi.arlock & m_axi.bresp != 2'b01;
-                write_outstanding <= ~(m_axi.bvalid & (~m_axi.arlock | m_axi.bresp == 2'b01));
-                if (m_axi.bvalid)
-                    current_state <= m_axi.arlock & m_axi.bresp != 2'b01 ? REQUESTING_READ : READY;
+                ls_output.ready <= m_axi_input.bvalid & (~m_axi_output.arlock | m_axi_input.bresp == 2'b01);
+                ls_output.data_out <= {31'b0, m_axi_input.bresp != 2'b01};
+                ls_output.data_valid <= m_axi_input.bvalid & m_axi_output.awlock & ~m_axi_output.arlock;
+                m_axi_output.arvalid <= m_axi_input.bvalid & m_axi_output.arlock & m_axi_input.bresp != 2'b01;
+                write_outstanding <= ~(m_axi_input.bvalid & (~m_axi_output.arlock | m_axi_input.bresp == 2'b01));
+                if (m_axi_input.bvalid)
+                    current_state <= m_axi_output.arlock & m_axi_input.bresp != 2'b01 ? REQUESTING_READ : READY;
             end
         endcase
         if (rst)
