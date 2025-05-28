71307f093767d0ad91507df758609d964d069574
annalia01 <annalia.ruggiero01@gmail.com>
2025-05-28 11:54:37 +0200

    Update riscv_types.sv

--- core/types_and_interfaces/riscv_types.sv
+++ core/types_and_interfaces/riscv_types.sv
@@ -26,28 +26,10 @@ package riscv_types;
     localparam XLEN = 32;
     localparam PAGE_ADDR_W = 12;
     localparam ECODE_W = 5;
-    localparam NUM_SUB_UNITS = int'(CONFIG.INCLUDE_ILOCAL_MEM) + int'(CONFIG.INCLUDE_ICACHE) + int'(CONFIG.INCLUDE_IBUS);
-    localparam NUM_SUB_UNITS_W = (NUM_SUB_UNITS == 1) ? 1 : $clog2(NUM_SUB_UNITS);
+   
 
     typedef logic [4:0] rs_addr_t;
-    typedef struct packed{
-        logic [XLEN-1:0] unsigned_dividend;
-        logic [XLEN-1:0] unsigned_divisor;
-        logic [$clog2(32)-1:0] dividend_CLZ;
-        logic [$clog2(32)-1:0] divisor_CLZ;
-        logic divisor_is_zero;
-        logic reuse_result;
-        div_attributes_t attr;
-    } div_fifo_inputs_t;
     
-     typedef struct packed{
-        logic is_predicted_branch_or_jump;
-        logic is_branch;
-        logic [31:0] early_flush_pc;
-        logic address_valid;
-        logic mmu_fault;
-        logic [NUM_SUB_UNITS_W-1:0] subunit_id;
-    } fetch_attributes_t;
     typedef struct packed {
         logic [6:0] fn7;
         logic [4:0] rs2_addr;
